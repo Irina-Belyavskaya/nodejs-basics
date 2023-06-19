25f5bd393c1879a091f7e2f6ad51073fb2a8f100
Irina-Belyavskaya <ir200210b@mail.ru>
2023-06-19 13:52:50 +0300

    add: implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import path from 'path';
+import { createWriteStream } from "fs";
+
 const write = async () => {
-    // Write your code here 
+    const filePath = path.resolve('files', 'fileToWrite.txt');
+    const writeStream = createWriteStream(filePath, { flags: 'a' });
+    process.stdin.pipe(writeStream);
+
+    writeStream.on('error', (error) => {
+        console.error('Error:', error);
+    });
 };
 
 await write();
