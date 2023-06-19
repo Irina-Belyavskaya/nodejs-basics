b0f49c8afaf1cbd6966810b98620e32e9b986ca6
Irina-Belyavskaya <ir200210b@mail.ru>
2023-06-19 12:47:33 +0300

    add: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
-const rename = async () => {
-    // Write your code here 
+import { access, constants, rename} from 'node:fs';
+import path from 'path';
+
+const renameFile = async () => {
+    const oldFileName = path.resolve('files', 'wrongFilename.txt');
+    const newFileName = path.resolve('files', 'properFilename.md');
+
+    access(oldFileName, constants.F_OK, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        } else {
+            access(newFileName, constants.F_OK, (err) => {
+                if (!err) {
+                    throw new Error('FS operation failed');
+                } else {
+                    rename(oldFileName, newFileName, (err) => {
+                        if (err)
+                            throw new Error(err);
+                    });
+                }
+            }); 
+        }
+    });    
 };
 
-await rename();
+await renameFile();
