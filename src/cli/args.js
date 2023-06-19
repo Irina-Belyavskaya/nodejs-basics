5388daacd530abff5d4ef53e9dcef678efd23673
Irina-Belyavskaya <ir200210b@mail.ru>
2023-06-19 13:21:47 +0300

    add: implement args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    var args = process.argv.slice(2);
+    const prefix = '--';
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].startsWith(prefix))
+            console.log(`${args[i].substring(2)} is ${args[i + 1]}`);
+    }
 };
 
 parseArgs();
