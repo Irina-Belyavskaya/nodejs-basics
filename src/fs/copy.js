62c4ac97784aca8e04151ee0da6f4de91183dfc6
Irina-Belyavskaya <ir200210b@mail.ru>
2023-06-19 12:28:48 +0300

    fix: rename variables

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -3,30 +3,27 @@ import path from 'path';
 
 const copy = async () => {
     
-    const dir = 'files';
-    const folderName = 'files_copy';
+    const folderName = 'files';
+    const newFolderName = 'files_copy';
 
-    access(dir, constants.F_OK, (err) => {
+    access(folderName, constants.F_OK, (err) => {
         if (err) {
             throw new Error('FS operation failed');
         } else {
-            access(folderName, constants.F_OK, (err) => {
+            access(newFolderName, constants.F_OK, (err) => {
                 if (err) {
-                    mkdirSync(folderName);
-                    readdir(dir, (err, list) => {
+                    mkdirSync(newFolderName);
+                    readdir(folderName, (err, list) => {
                         if (err) {
                           callback(err);
                           return;
                         }
                         list.forEach((item) => {
-                            const curSrc = path.resolve(dir, item);
-                            const curDest = path.resolve(folderName, item);
-                            createReadStream(curSrc).pipe(createWriteStream(curDest));
+                            const src = path.resolve(folderName, item);
+                            const dest = path.resolve(newFolderName, item);
+                            createReadStream(src).pipe(createWriteStream(dest));
                         });
-                        
                     });
-
-                    
                 } else {
                     throw new Error('FS operation failed');
                 }
