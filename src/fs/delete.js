d61bec7dc3a62525a79f15ad7ef4e105b6a655f0
Irina-Belyavskaya <ir200210b@mail.ru>
2023-06-19 12:53:02 +0300

    add: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { access, constants, rm} from 'node:fs';
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+    const fileName = path.resolve('files', 'fileToRemove.txt');
+
+    access(fileName, constants.F_OK, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        } else {
+            rm(fileName, (err) => { 
+                if (err) {
+                    throw new Error(err);
+                }
+            });
+        }
+    });    
 };
 
 await remove();
