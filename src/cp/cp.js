e789468d1ed4a26c1520a313269d0ad757a60e13
Irina-Belyavskaya <ir200210b@mail.ru>
2023-06-19 14:45:40 +0300

    add: implement spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { spawn } from 'child_process';
+import path from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const filePath = path.resolve('files', 'script.js');
+    const child = spawn('node', [filePath, ...args], { stdio: 'pipe' });
+    
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
+    child.on('error', (error) => {
+        console.error('Child error:', error);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
