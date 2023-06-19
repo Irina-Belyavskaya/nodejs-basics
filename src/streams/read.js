390485916006ad4d70ad4f7542f02839e22f1fc6
Irina-Belyavskaya <ir200210b@mail.ru>
2023-06-19 13:50:17 +0300

    add: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import path from 'path';
+import { createReadStream } from "fs";
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.resolve('files', 'fileToRead.txt');
+    const readStream = createReadStream(filePath, 'utf8');
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('error', (error) => {
+        console.error('Error:', error);
+    });
 };
 
 await read();
