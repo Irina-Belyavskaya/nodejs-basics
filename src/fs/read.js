5463bad6b2bf438f771851934eb225c33baa6eaf
Irina-Belyavskaya <ir200210b@mail.ru>
2023-06-19 13:00:21 +0300

    add: implement function read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { access, constants, readFile } from 'node:fs';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.resolve('files', 'fileToRead.txt');
+
+    access(filePath, constants.F_OK, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        } else {
+            readFile(filePath, 'utf8', function(err, data) {
+                if (err) 
+                    throw new Error(err);
+                console.log(data);
+            });
+        }
+    });   
 };
 
 await read();
