abb014c6f53334db39757ec8a40a46827dcfe2f0
Irina-Belyavskaya <ir200210b@mail.ru>
2023-06-19 13:11:56 +0300

    add: implement env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    process.env.RSS_name1 = 1;
+    process.env.RSS_name2 = 2;
+
+    const prefix = 'RSS_';
+    const envVariables = Object.keys(process.env).filter((key) => {
+        return key.startsWith(prefix);
+    });
+    envVariables.forEach((key) => {
+        const value = process.env[key];
+        console.log(`${key}=${value}`);
+    });
 };
 
 parseEnv();
