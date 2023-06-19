aeca6a312fd4fcd950543e8b56df7ac3230f7de2
Irina-Belyavskaya <ir200210b@mail.ru>
2023-06-19 12:26:25 +0300

    add: make copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,38 @@
+import { access, constants, readdir, mkdirSync, createReadStream, createWriteStream} from 'node:fs';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    
+    const dir = 'files';
+    const folderName = 'files_copy';
+
+    access(dir, constants.F_OK, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        } else {
+            access(folderName, constants.F_OK, (err) => {
+                if (err) {
+                    mkdirSync(folderName);
+                    readdir(dir, (err, list) => {
+                        if (err) {
+                          callback(err);
+                          return;
+                        }
+                        list.forEach((item) => {
+                            const curSrc = path.resolve(dir, item);
+                            const curDest = path.resolve(folderName, item);
+                            createReadStream(curSrc).pipe(createWriteStream(curDest));
+                        });
+                        
+                    });
+
+                    
+                } else {
+                    throw new Error('FS operation failed');
+                }
+            });
+        }
+    });    
 };
 
 await copy();
